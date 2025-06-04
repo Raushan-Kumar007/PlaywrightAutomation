b566940a6e9d5684d1b23384d5a61a7eb5f451b4
Raushan-Kumar007 <ravisankaranand199@gmail.com>
2025-06-04 13:42:35 +0530

    Update Yml file

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -12,9 +12,7 @@ test('Login to Salesforce as main user', async ({ page }) => {
   await expect(page).toHaveURL(/lightning/);
 });
 test('Login to Salesforce as main user2', async ({ page }) => {
-  // const username = process.env.SF_USERNAME!;
   const clientId = process.env.SF_CLIENT_ID!;
-
   await loginAsUser(page, "frank123@gmail.com", clientId);
   await expect(page).toHaveURL(/lightning/);
 });
